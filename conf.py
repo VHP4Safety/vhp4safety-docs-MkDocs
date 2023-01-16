ef00e2e0e37f09ee0d8c31910af7790ddfbb4105
Marc Teunis <marc.teunis@hu.nl>
2023-01-16 21:18:28 +0100

    initial commit

---
+++ conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'VHP4Safety-Docs'
+copyright = '2023, Marc Teunis, PhD; Ozan Cinar, PhD; Egon Willigenhagen, PhD'
+author = 'Marc Teunis, PhD; Ozan Cinar, PhD; Egon Willigenhagen, PhD'
+release = '0.0.1.9000'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,20 @@
+.. VHP4Safety-Docs documentation master file, created by
+   sphinx-quickstart on Mon Jan 16 20:52:49 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to VHP4Safety-Docs's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
