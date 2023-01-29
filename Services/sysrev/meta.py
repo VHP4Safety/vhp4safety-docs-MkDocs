3d545722e12807c739ffd87cf18ab9ba0dcd729a
Marc Teunis <marc.teunis@hu.nl>
2023-01-29 20:39:35 +0100

    update

--- Services/sysrev/meta.py
+++ Services/sysrev/meta.py
@@ -2,5 +2,5 @@ import json
 import pandas as pd
 
 df = pd.read_json("https://raw.githubusercontent.com/VHP4Safety/cloud/main/docs/service/sysrev.json")
-df.to_html('Services/cdk_depict/meta.html')
+df.to_html('Services/sysrev/meta.html')
 print(df)
